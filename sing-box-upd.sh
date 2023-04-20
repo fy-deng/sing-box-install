a1670294aa9699e7aab28987ab6682e4579405a8
fy-deng <46555482+fy-deng@users.noreply.github.com>
2023-04-20 10:32:14 +0800

    Create sing-box-upd.sh

---
+++ sing-box-upd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 检查系统架构
+ARCH=$(uname -m)
+
+# 根据架构执行命令
+if [[ "$ARCH" == "x86_64" || "$ARCH" == "amd64" ]]; then
+  echo "Detected AMD64 architecture. Download sing-box now."
+  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.2.4/sing-box-1.2.4-linux-amd64.tar.gz && \
+  tar -xzf ~/sb.tar.gz -C ~/ && \
+  sudo cp -f ~/sing-box-*/sing-box /usr/local/bin/ && \
+  rm -rf ~/sb.tar.gz ~/sing-box-*
+  
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+  echo "Detected ARM64 architecture. Download sing-box now."
+  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.2.4/sing-box-1.2.4-linux-arm64.tar.gz && \
+  tar -xzf ~/sb.tar.gz -C ~/ && \
+  sudo cp -f ~/sing-box-*/sing-box /usr/local/bin/ && \
+  rm -rf ~/sb.tar.gz ~/sing-box-*
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
