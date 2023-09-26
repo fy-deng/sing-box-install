3aaaf41b3b038cdc7d6e3e65bbc3ee8619ea2a49
fy-deng <46555482+fy-deng@users.noreply.github.com>
2023-09-26 17:12:09 +0800

    Update sing-box-upd.sh

--- sing-box-upd.sh
+++ sing-box-upd.sh
@@ -6,14 +6,14 @@ ARCH=$(uname -m)
 # 根据架构执行命令
 if [[ "$ARCH" == "x86_64" || "$ARCH" == "amd64" ]]; then
   echo "Detected AMD64 architecture. Download sing-box now."
-  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.4.3/sing-box-1.4.3-linux-amd64.tar.gz && \
+  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.4.5/sing-box-1.4.5-linux-amd64.tar.gz && \
   tar -xzf ~/sb.tar.gz -C ~/ && \
   sudo cp -f ~/sing-box-*/sing-box /usr/local/bin/ && \
   rm -rf ~/sb.tar.gz ~/sing-box-*
   
 elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
   echo "Detected ARM64 architecture. Download sing-box now."
-  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.4.3/sing-box-1.4.3-linux-arm64.tar.gz && \
+  curl -Lo ~/sb.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.4.5/sing-box-1.4.5-linux-arm64.tar.gz && \
   tar -xzf ~/sb.tar.gz -C ~/ && \
   sudo cp -f ~/sing-box-*/sing-box /usr/local/bin/ && \
   rm -rf ~/sb.tar.gz ~/sing-box-*
